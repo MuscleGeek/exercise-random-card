921d99a2fa9602f20838def60bf3dc3837e682bf
Jonathan Villalobos Mora <hackertristania@gmail.com>
2021-01-29 07:22:02 +0000

    working on minor fixes for auto-suit-color-deck

--- cardz.js
+++ cardz.js
@@ -1,6 +1,8 @@
+window.onload = () =>{
+
 /*--------BEGIN cards and numbers vectors*/
-let mycards = ['&spades;','&hearts;','&diams;','&clubs;'];
-let mycardnumbers = [2,3,4,5,6,7,8,9,10,J,Q,K,A];
+let myCards = ['&spades;','&clubs;','&diams;','&hearts;'];
+let myCardNumbers = ['2','3','4','5','6','7','8','9','10','J','Q','K','A'];
 
 /*-------END random cards and numbers */
 
@@ -9,22 +11,37 @@ let mycardnumbers = [2,3,4,5,6,7,8,9,10,J,Q,K,A];
 
 let randomNumbers = Math.floor(Math.random()* 13);
 
-let randomSharp = Math.floor(Math.random() * 4);
+let randomSharp = Math.floor(Math.random()* 4);
 /*-----END random cards and numbers Math.random */
 
-/*--------BEGIN gencard-number ----*/
-let cardSharp = mycards[randomCards];
-let numberGen = mycardnumbers[randomNumbers];
+/*--------BEGIN genCard-genNumber ----*/
+let cardSharp = myCards[randomSharp];
+let numberGen = myCardNumbers[randomNumbers];
 
-/*------- END gencard-number ---*/
+/*------- END genCard-genNumber ---*/
 
 /*------- BEGIN data => IDs -----*/
 
-let idHead = document.getElementsByClassName('head-path');
-    idHead.innerHTML = cardGen;
-let idBody = document.getElementsByClassName('main-path');
+let idHead = document.querySelector('.my-head');
+    idHead.innerHTML = cardSharp;
+    /*---BEGIN Red Ones ---
+if(myCards == myCards[myCardNumbers[3]] || myCards == myCards[myCards[4]]){
+    document.querySelector('.my-header').style.color = 'red';
+    document.querySelector('.my-footer').style.color = 'red';
+}
+    /*---END Red Ones ---*/
+let idBody = document.querySelector('h1');
     idBody.innerHTML = numberGen;
-let idFooter = document.getElementsByClassName('footer-path');
-    idFooter.innerHTML = cardGen;
+let idFooter = document.querySelector('.my-footer');
+    idFooter.innerHTML = cardSharp;
+    /*---BEGIN Black Ones---
+    if(myCards == myCards[myCards[0]] || myCards == myCards[myCards[1]]){
+    document.querySelector('my-header').style.color = 'black';
+    document.querySelector('my-footer').style.color = 'black';
+    }
+    /*---END Black Ones---*/
 
 /*----- END data => IDs ----*/
+/*-----BEGIN Red Ones or Black Ones----*/
+
+/*-----END Red Ones or Black Ones----*/
